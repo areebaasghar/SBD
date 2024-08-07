c758a9016d93e595c454da202351a6ddffe7b4d9
Areeba Asghar <areebaasghar1515@gmail.com>
2024-08-07 23:28:02 +0500

    added survey view

---
+++ SBDModelClass/EntTemplate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBDModelClass
+{
+    public class EntTemplate
+    {
+        public string? QuestionText { get; set; }
+        public string? QuestionType { get; set; }
+        public string? OptionText {  get; set; }
+
+    }
+}
